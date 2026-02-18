98fc85b4fe09df06ff5052f1f3c3b22fb1ce4173
theWhiteFox <stethewhitefox@gmail.com>
2026-02-18 17:28:55 +0000

    useMemo cart items

--- app/ui/products/product-cart.tsx
+++ app/ui/products/product-cart.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useMemo } from 'react'
+import { useRouter } from 'next/navigation'
 import { ProductCard } from "./product-card"
 import useCartStore from "@/app/store/cartStore"
 import clsx from "clsx"
@@ -7,19 +9,27 @@ import { Button } from "../button"
 import { ArrowLeftIcon } from "@heroicons/react/16/solid"
 
 export const ProductCart = () => {
-    const { items } = useCartStore((state) => state)
-    const subtotal = items.reduce(
-        (total, item) => total + item.price * item.quantity,
-        0
-    )
+    // Only select "items" to prevent re-renders
+    const items = useCartStore((state) => state.items)
+    const router = useRouter()
 
-    const total = subtotal
+    const subtotal = useMemo(() => {
+        return items.reduce((total, item) => total + item.price * item.quantity, 0)
+    }, [items])
+
+    const totalQuantity = useMemo(() => {
+        return items.reduce(
+            (sum, i) => sum + i.price * i.quantity,
+            0
+        )
+    }, [items])
+
 
     return (
         <>
             <div className="p-4">
                 <Button
-                    onClick={() => window.location.href = '/'}
+                    onClick={() => router.push('/')}
                     className={clsx(
                         "w-32 mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold hover:text-white rounded py-2 px-4",
                         "hover:outline-none focus:shadow-outline text-white-500"
@@ -29,7 +39,7 @@ export const ProductCart = () => {
                     <span className="pl-1">Products</span>
 
                 </Button>
-                <h2 className="py-8 text-2xl font-bold">Cart ({items.reduce((sum, i) => sum + i.quantity, 0)})
+                <h2 className="py-8 text-2xl font-bold">Cart Cart ({totalQuantity})
                 </h2>
                 {items.length > 0 ? items.map((item) => (
                     <ProductCard
@@ -45,7 +55,7 @@ export const ProductCart = () => {
             </div>
             <div className="flex justify-between font-semibold text-lg mt-4 p-4 border-t">
                 <span>Total</span>
-                <span>€{total.toFixed(2)}</span>
+                <span>€{subtotal.toFixed(2)}</span>
             </div>
         </>
     )
